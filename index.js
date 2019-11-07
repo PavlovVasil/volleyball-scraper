6c6fe23561297b46a3af24bc8e127ed423400b49
PavlovVasil <vasil@vasilpavlov.com>
2019-11-07 23:11:43 +0200

    Get all table rows and table title

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const cheerio = require('cheerio');
+const axios = require('axios');
 
-(async() => {
-    
+(async () => {
+    try {
+        const response = await axios.get('https://volleymania.com/standings');
+        //loading response in cheerio
+        const $ = cheerio.load(response.data);
+        const mainRankingTitle = $('#content .mainContent h3')[0];
+        const allTableRows = Array.from($('#content .mainContent table tbody tr'))
+        allTableRows.unshift(mainRankingTitle)
+        console.log(allTableRows)
+    } catch (err) {
+        console.log(err)
+    }
 })()
