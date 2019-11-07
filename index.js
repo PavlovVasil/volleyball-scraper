b3e15076ae1c690d2fc708d767255571119cc92a
PavlovVasil <vasil@vasilpavlov.com>
2019-11-07 23:57:27 +0200

    Change variable and function names

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const cheerio = require('cheerio');
 const axios = require('axios');
 
-//take nodeList and break it down into several separate tables
-const breakIntoChunks = ($, nodeArr, selector) => {
+//take nodeList and break it down into several separate tables. The first element in each table is its header.
+const breakIntoTables = ($, nodeListArray, selector) => {
     const result = [];
-    let currentChunk = [];
-    for (let i = 0; i < nodeArr.length; i++) {
+    let currentTable = [];
+    for (let i = 0; i < nodeListArray.length; i++) {
         //if this is not a title
-        if ($(nodeArr[i]).attr('bgcolor') !== selector) {
-            currentChunk.push(nodeArr[i]);
+        if ($(nodeListArray[i]).attr('bgcolor') !== selector) {
+            currentTable.push(nodeListArray[i]);
             //if this is the last item, add the last chunk
-            if (i === nodeArr.length -1){
-                result.push(currentChunk);
+            if (i === nodeListArray.length -1){
+                result.push(currentTable);
             }
         } else {
             //add chunk, empty it and add he current title to the next chunk
-            result.push(currentChunk);
-            currentChunk = [];
-            currentChunk.push(nodeArr[i]);
+            result.push(currentTable);
+            currentTable = [];
+            currentTable.push(nodeListArray[i]);
         }
     }
     return result;
@@ -31,7 +31,7 @@ const breakIntoChunks = ($, nodeArr, selector) => {
         const mainRankingTitle = $('#content .mainContent h3')[0];
         const allTableRows = Array.from($('#content .mainContent table tbody tr'));
         allTableRows.unshift(mainRankingTitle);
-        const tables = breakIntoChunks($, allTableRows, "#FFFFFF");
+        const tables = breakIntoTables($, allTableRows, "#FFFFFF");
         
         console.log(tables)
     } catch (err) {
